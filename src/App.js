40d4aa9f1ed3d91e9152fc8f1dc9bd1c1f17dd6c
Amanda Garcia <algarcian@outlook.com>
2019-06-06 10:00:50 -0600

    Fixed parsing error

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import {Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Home'
 
-import React from 'react'
-
 // will act as a routing station to other components instead of acting as the main page. 
 const App = () => (
   <Route exact path="/" component={Home}/>
